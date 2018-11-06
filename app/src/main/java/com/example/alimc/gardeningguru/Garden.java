11718dea5090d4a5abb7d0d16d07f698d14adae6
Alvern Woods <alvern.woods@gmail.com>
2018-11-06 10:29:36 -0700

    Added garden

---
+++ app/src/main/java/com/example/alimc/gardeningguru/Garden.java
@@ -0,0 +1,16 @@
+package com.example.alimc.gardeningguru;
+
+public class Garden {
+
+    private String name;
+    //private Zone zone;
+    //private Map<String, Plant> plants;
+    //private Map<String, Plantings> plantings;
+    //private Map<Date, Task> tasksPending;
+    //private Map<Date, Task> tasksComplete;
+
+    public Garden(String name) {this.name = name; }
+
+
+
+}
