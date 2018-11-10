e0a7fa8f6259b677f172a6a3f234c991a352e584
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2018-11-10 14:35:04 -0800

    renamed xml file to match naming conventions

--- app/src/main/java/com/example/alimc/gardeningguru/ZoneLookup.java
+++ app/src/main/java/com/example/alimc/gardeningguru/ZoneLookup.java
@@ -1,18 +1,10 @@
 package com.example.alimc.gardeningguru;
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class ZoneLookup extends AppCompatActivity {
     TextView ZoneView;
@@ -25,7 +17,7 @@ public class ZoneLookup extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zoneLookup);
+        setContentView(R.layout.activity_zonelookup);
         // create button function
         Button btnLookup = findViewById(R.id.submitZip);
         //create textView function
