d5cf5e6ea94b61ecefaa4fd1ddf70dc9496b2ebf
matthew <mar17132@byui.edu>
2018-11-26 16:07:22 -0700

    Unused classes

--- app/src/main/java/com/example/alimc/gardeningguru/AddPlant.java
+++ app/src/main/java/com/example/alimc/gardeningguru/AddPlant.java
@@ -2,9 +2,7 @@ package com.example.alimc.gardeningguru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
