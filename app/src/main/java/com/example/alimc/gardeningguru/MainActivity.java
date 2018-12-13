ddfa82f87fd0eee6e88c81a7d884b56ddfe4400c
matthew <mar17132@byui.edu>
2018-12-12 20:24:13 -0700

    task on main activity

--- app/src/main/java/com/example/alimc/gardeningguru/MainActivity.java
+++ app/src/main/java/com/example/alimc/gardeningguru/MainActivity.java
@@ -6,13 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializer;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     public static SharedPreferences mPrefs;
     TextView displayZone;
 
+    private TaskAdapterMainAct taskMainAdapter;
+    private List<Task> taskListMain;
+    private ListView taskListViewMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         displayZone = findViewById(R.id.viewZone);
         //initialize garden, or load garden if one already exists
         mPrefs = this.getPreferences(MODE_PRIVATE);
+
+        this.taskListViewMain = (ListView) findViewById(R.id.viewToDo);
 
         if (!mPrefs.contains("garden")) { // No garden exists in shared preferences.
             garden = new Garden(GARDEN_NAME);
@@ -67,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
             garden.loadGarden();
         }
         garden.computeTasksPending();
+        fillTaskList();
     }
     public void launchZoneLookup(View v) {
         Intent intent = new Intent(this, ZoneLookup.class);
@@ -110,6 +121,22 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
    }
 
+    public void fillTaskList() {
+
+        if (garden.getTasksPending() != null) {
+            this.taskListMain = new ArrayList<>(garden.getTasksPending().values());
+            this.taskMainAdapter = new TaskAdapterMainAct(this, this.taskListMain);
+            this.taskListViewMain.setAdapter(this.taskMainAdapter);
+            this.taskListViewMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent(parent.getContext(), TaskList.class);
+                    parent.getContext().startActivity(intent);
+                }
+            });
+        }
+
+    }
 
 
 }
---
+++ app/src/main/java/com/example/alimc/gardeningguru/TaskAdapterMainAct.java
@@ -0,0 +1,40 @@
+package com.example.alimc.gardeningguru;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TaskAdapterMainAct extends ArrayAdapter<Task> {
+
+    private List<Task> taskArrayList;
+
+    public TaskAdapterMainAct(Context context, List<Task> tasks){
+
+        super(context,0,tasks);
+        this.taskArrayList = tasks;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, final ViewGroup parent){
+
+        Task task = getItem(position);
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.task_list_layout_main_act,parent,false);
+        }
+
+        TextView taskName = (TextView) convertView.findViewById(R.id.taskNameTxtMain);
+
+        taskName.setText(task.getName());
+
+        return convertView;
+
+    }
+
+
+}
